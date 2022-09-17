c9aca19d7bea1fd0b4b039e92a54d2b6ed8a7b15
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-09-17 20:18:44 +0800

    feat(challenge): #200
    Co-Authored-By: Bingjie YAN <bj.yan.pa@qq.com>

--- src/services/settings.py
+++ src/services/settings.py
@@ -11,7 +11,7 @@ from services.utils import ToolBox
 HCAPTCHA_DEMO_API = "https://accounts.hcaptcha.com/demo?sitekey={}"
 _SITE_KEYS = {
     "epic": "91e4137f-95af-4bc9-97af-cdcedce21c8c",
-    "hcaptcha": "00000000-0000-0000-0000-000000000000",
+    "hcaptcha": "a5f74b19-9e45-40e0-b45d-47ff91b7a6c2",
     "discord": "f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34",
     "oracle": "d857545c-9806-4f9e-8e9d-327f565aeb46",
     "publisher": "c86d730b-300a-444c-a8c5-5312e7a93628",
