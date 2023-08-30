bc24833ec6437dd70ff9945d17f7c0bbb556f63e
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-08-31 02:22:52 +0800

    Perf(agent): change to asyncio style

--- hcaptcha_challenger/agents/playwright/tarnished.py
+++ hcaptcha_challenger/agents/playwright/tarnished.py
@@ -6,12 +6,29 @@
 from __future__ import annotations
 
 import inspect
-from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, Callable, Awaitable, List
 
 from loguru import logger
+from playwright.async_api import BrowserContext as ASyncContext, async_playwright
 from playwright.sync_api import BrowserContext as SyncContext, sync_playwright
+
+enabled_evasions = [
+    "chrome.app",
+    "chrome.csi",
+    "chrome.loadTimes",
+    "chrome.runtime",
+    "iframe.contentWindow",
+    "media.codecs",
+    "navigator.hardwareConcurrency",
+    "navigator.languages",
+    "navigator.permissions",
+    "navigator.plugins",
+    "navigator.webdriver",
+    "sourceurl",
+    "webgl.vendor",
+    "window.outerdimensions",
+]
 
 
 class Tarnished:
@@ -29,24 +46,7 @@ class Tarnished:
         self.state_path = state_path
 
     @staticmethod
-    def _apply_stealth(context: SyncContext):
-        enabled_evasions = [
-            "chrome.app",
-            "chrome.csi",
-            "chrome.loadTimes",
-            "chrome.runtime",
-            "iframe.contentWindow",
-            "media.codecs",
-            "navigator.hardwareConcurrency",
-            "navigator.languages",
-            "navigator.permissions",
-            "navigator.plugins",
-            "navigator.webdriver",
-            "sourceurl",
-            "webgl.vendor",
-            "window.outerdimensions",
-        ]
-
+    def apply_stealth(context: SyncContext):
         for e in enabled_evasions:
             evasion_code = (
                 Path(__file__)
@@ -57,17 +57,6 @@ class Tarnished:
 
         return context
 
-    @staticmethod
-    def _patch_cookies(context: SyncContext):
-        five_days_ago = datetime.now() - timedelta(days=5)
-        cookie = {
-            "name": "OptanonAlertBoxClosed",
-            "value": five_days_ago.isoformat(),
-            "domain": ".epicgames.com",
-            "path": "/",
-        }
-        context.add_cookies([cookie])
-
     def storage_state(self, context: SyncContext):
         if self.state_path:
             logger.info("Storage ctx_cookie", path=self.state_path)
@@ -75,7 +64,7 @@ class Tarnished:
 
     def execute(
         self,
-        sequence,
+        sequence: List | Callable[..., None],
         *,
         parameters: Dict[str, Any] = None,
         headless: bool = False,
@@ -92,8 +81,7 @@ class Tarnished:
                 args=["--hide-crash-restore-bubble"],
                 **kwargs,
             )
-            self._apply_stealth(context)
-            self._patch_cookies(context)
+            self.apply_stealth(context)
 
             if not isinstance(sequence, list):
                 sequence = [sequence]
@@ -110,3 +98,72 @@ class Tarnished:
                 else:
                     container(context, **kws)
             context.close()
+
+
+class Malenia:
+    def __init__(
+        self,
+        user_data_dir: Path,
+        *,
+        record_dir: Path | None = None,
+        record_har_path: Path | None = None,
+        state_path: Path | None = None,
+    ):
+        self._user_data_dir = user_data_dir
+        self._record_dir = record_dir
+        self._record_har_path = record_har_path
+        self.state_path = state_path
+
+    @staticmethod
+    async def apply_stealth(context: ASyncContext):
+        for e in enabled_evasions:
+            evasion_code = (
+                Path(__file__)
+                .parent.joinpath(f"puppeteer-extra-plugin-stealth/evasions/{e}/index.js")
+                .read_text(encoding="utf8")
+            )
+            await context.add_init_script(evasion_code)
+
+        return context
+
+    async def storage_state(self, context: ASyncContext):
+        if self.state_path:
+            logger.info("Storage ctx_cookie", path=self.state_path)
+            await context.storage_state(path=self.state_path)
+
+    async def execute(
+        self,
+        sequence: Callable[..., Awaitable[...]] | List,
+        *,
+        parameters: Dict[str, Any] = None,
+        headless: bool = False,
+        locale: str = "en-US",
+        **kwargs,
+    ):
+        async with async_playwright() as p:
+            context = await p.firefox.launch_persistent_context(
+                user_data_dir=self._user_data_dir,
+                headless=headless,
+                locale=locale,
+                record_video_dir=self._record_dir,
+                record_har_path=self._record_har_path,
+                args=["--hide-crash-restore-bubble"],
+                **kwargs,
+            )
+            await self.apply_stealth(context)
+
+            if not isinstance(sequence, list):
+                sequence = [sequence]
+            for container in sequence:
+                logger.info("Execute task", name=container.__name__)
+                kws = {}
+                params = inspect.signature(container).parameters
+                if parameters and isinstance(parameters, dict):
+                    for name in params:
+                        if name != "context" and name in parameters:
+                            kws[name] = parameters[name]
+                if not kws:
+                    await container(context)
+                else:
+                    await container(context, **kws)
+            await context.close()
