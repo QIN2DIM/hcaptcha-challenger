ce439fe575d931111602bf4ad67852124492c110
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-02 03:52:47 +0800

    Update(demo): startup playwright with chromium browser (#620)

---
+++ examples/demo_normal_playwright.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/9/2 3:30
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+from __future__ import annotations
+
+import asyncio
+from pathlib import Path
+
+from loguru import logger
+from playwright.async_api import BrowserContext as ASyncContext, async_playwright
+
+import hcaptcha_challenger as solver
+from hcaptcha_challenger.agents.playwright.control import AgentT
+from hcaptcha_challenger.utils import SiteKey
+
+# Init local-side of the ModelHub
+solver.install(upgrade=True, flush_yolo=False)
+
+# Save dataset to current working directory
+tmp_dir = Path(__file__).parent.joinpath("tmp_dir")
+
+
+@logger.catch
+async def hit_challenge(context: ASyncContext, times: int = 8):
+    page = await context.new_page()
+    agent = AgentT.from_page(page=page, tmp_dir=tmp_dir)
+    await page.goto(SiteKey.as_sitelink(sitekey="hcaptcha"))
+
+    await agent.handle_checkbox()
+
+    for pth in range(1, times):
+        result = await agent()
+        print(f">> {pth} - Challenge Result: {result} - question={agent.qr.requester_question}")
+        match result:
+            case agent.status.CHALLENGE_BACKCALL:
+                await page.wait_for_timeout(500)
+                fl = page.frame_locator(agent.HOOK_CHALLENGE)
+                await fl.locator("//div[@class='refresh button']").click()
+            case agent.status.CHALLENGE_SUCCESS:
+                return
+
+
+async def bytedance():
+    # playwright install chromium
+    # playwright install-deps chromium
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(locale="en-US")
+        await hit_challenge(context)
+
+
+if __name__ == "__main__":
+    asyncio.run(bytedance())
