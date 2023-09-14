3b79d3676f656ebc497f14eb0400bb72be087ad4
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-14 13:50:47 +0800

    Create test_normal_playwright.py

---
+++ tests/test_normal_playwright.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/9/14 13:32
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+from __future__ import annotations
+
+import pytest
+from playwright.async_api import async_playwright
+
+import hcaptcha_challenger as solver
+from hcaptcha_challenger.utils import SiteKey
+
+pytest_plugins = ("pytest_asyncio",)
+
+# Init local-side of the ModelHub
+solver.install(upgrade=True, flush_yolo=False)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("sitekey", [SiteKey.epic, SiteKey.discord, SiteKey.user_easy])
+@pytest.mark.parametrize("times", [3])
+async def test_normal_playwright(sitekey: str, times: int):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+
+        agent = solver.AgentT.from_page(page=page)
+        await page.goto(SiteKey.as_sitelink(sitekey))
+
+        await agent.handle_checkbox()
+
+        for pth in range(1, times):
+            result = await agent()
+            if result in [agent.status.CHALLENGE_SUCCESS]:
+                return
+            if result in [agent.status.CHALLENGE_RETRY]:
+                continue
+            assert (
+                result != agent.status.CHALLENGE_BACKCALL
+            ), f">> {pth} - Challenge Result: {result} - question={agent.qr.requester_question}"
