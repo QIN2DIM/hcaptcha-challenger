f96a1336608146aa856b4f8c618dd07dee58d5db
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-15 02:19:14 +0800

    k

--- automation/istock.py
+++ automation/istock.py
@@ -7,24 +7,21 @@ import asyncio
 from pathlib import Path
 from typing import Tuple
 
-import pytest
 from istockphoto import Istock
 
 tmp_dir = Path(__file__).parent
 
-phrases = ["airplane", "boat", "coffee"]
+phrases = ["cat"]
 name2similar = [("hummingbird", "108176594")]
 
 
-@pytest.mark.parametrize("phrase", phrases)
-def test_select_phrase(phrase: str):
+def select_phrase(phrase: str):
     istock = Istock.from_phrase(phrase, tmp_dir)
-    istock.pages = 2
+    istock.pages = 4
     asyncio.run(istock.mining())
 
 
-@pytest.mark.parametrize("phrase_with_id", name2similar)
-def test_similar_phrase(phrase_with_id: Tuple[str, str] | None):
+def similar_phrase(phrase_with_id: Tuple[str, str] | None):
     if not phrase_with_id:
         return
 
@@ -33,3 +30,7 @@ def test_similar_phrase(phrase_with_id: Tuple[str, str] | None):
     istock.pages = 2
     istock.more_like_this(istock_id)
     asyncio.run(istock.mining())
+
+
+if __name__ == '__main__':
+    select_phrase("elephant")
