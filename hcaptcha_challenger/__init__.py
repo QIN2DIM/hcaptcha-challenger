595b02c3f8526a379d8b01d0f2edf1076179947e
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-10-05 19:59:06 +0800

    Update(utils): install --flush-yolo (#792)
    * Update(utils): install --flush-yolo
    
    * Update test_install.py

--- hcaptcha_challenger/__init__.py
+++ hcaptcha_challenger/__init__.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Iterable
 from urllib.parse import urlparse
 
 from hcaptcha_challenger.components.image_classifier import Classifier as BinaryClassifier
@@ -18,6 +19,7 @@ from hcaptcha_challenger.components.prompt_handler import (
     split_prompt_message,
     prompt2task,
 )
+from hcaptcha_challenger.onnx.modelhub import DEFAULT_KEYPOINT_MODEL
 from hcaptcha_challenger.onnx.modelhub import ModelHub
 from hcaptcha_challenger.onnx.resnet import ResNetControl
 from hcaptcha_challenger.onnx.yolo import YOLOv8
@@ -32,6 +34,7 @@ __all__ = [
     "split_prompt_message",
     "prompt2task",
     "ModelHub",
+    "DEFAULT_KEYPOINT_MODEL",
     "ResNetControl",
     "YOLOv8",
     "install",
@@ -57,15 +60,26 @@ def install(
     upgrade: bool | None = False,
     username: str = "QIN2DIM",
     lang: str = "en",
-    flush_yolo: bool = False,
+    flush_yolo: bool | Iterable[str] = False,
 ):
     modelhub = ModelHub.from_github_repo(username=username, lang=lang)
     modelhub.pull_objects(upgrade=upgrade)
     modelhub.assets.flush_runtime_assets(upgrade=upgrade)
-    if flush_yolo:
+
+    if flush_yolo is not None:
         from hcaptcha_challenger.onnx.modelhub import DEFAULT_KEYPOINT_MODEL
 
-        modelhub.pull_model(focus_name=DEFAULT_KEYPOINT_MODEL)
+        modelhub.parse_objects()
+
+        if isinstance(flush_yolo, bool) and flush_yolo:
+            flush_yolo = [DEFAULT_KEYPOINT_MODEL]
+        if isinstance(flush_yolo, Iterable):
+            pending_models = []
+            for model_name in flush_yolo:
+                if model_name in modelhub.ashes_of_war:
+                    modelhub.pull_model(model_name)
+                    pending_models.append(model_name)
+            return pending_models
 
 
 def set_reverse_proxy(https_cdn: str):
---
+++ tests/test_install.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/10/5 17:36
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+import os
+import typing
+
+import httpx
+import pytest
+
+import hcaptcha_challenger as solver
+from hcaptcha_challenger import ModelHub
+
+
+@pytest.mark.parametrize(
+    ["upgrade", "flush_yolo"],
+    [
+        [False, "e"],
+        [False, "0"],
+        [False, "1"],
+        [False, 0],
+        [False, 1],
+        [False, -0.1],
+        [False, False],
+        [False, True],
+        [False, "head_of_the_animal_bear_2309_yolov8n"],
+        [False, "head_of_the_animal_bear_2309_yolov8n.onnx"],
+        [False, ("head_of_the_animal_bear_2309_yolov8n.onnx",)],
+        [False, ["head_of_the_animal_bear_2309_yolov8n.onnx"]],
+        # then create objects.yaml
+        [True, True],
+        [True, False],
+        [True, "head_of_the_animal_bear_2309_yolov8n"],
+        [True, "head_of_the_animal_bear_2309_yv8n"],
+        [True, "head_onimal_bear_2309_yv8n"],
+        [True, "head_onimal_b9_yv8n"],
+        [True, "head_of_the_animal_bear_2309_yolov8n.onnx"],
+        [True, ["head_of_the_animal_bear_2309_yolov8n.onnx"]],
+        [True, ["can_be_eaten_2312_yolov8s.onnx"]],
+    ],
+)
+def test_install(upgrade, flush_yolo):
+    try:
+        pending_models = solver.install(upgrade=upgrade, flush_yolo=flush_yolo)
+    # Too many requests for a single IP
+    except httpx.ReadTimeout:
+        return
+
+    modelhub = ModelHub()
+    modelhub.parse_objects()
+    files = os.listdir(modelhub.models_dir)
+
+    if isinstance(flush_yolo, bool):
+        if flush_yolo is False:
+            assert pending_models is None
+        elif flush_yolo is True:
+            assert isinstance(pending_models, list)
+            assert len(pending_models) == 1
+            if upgrade:
+                assert modelhub.models_dir.joinpath(pending_models[0]).exists()
+    elif isinstance(flush_yolo, typing.Iterable):
+        for fy in flush_yolo:
+            if fy in modelhub.ashes_of_war:
+                assert fy in pending_models
+                if upgrade:
+                    assert modelhub.models_dir.joinpath(fy).exists()
+            else:
+                if isinstance(pending_models, typing.Iterable):
+                    assert fy not in pending_models
+                    assert fy not in files
+                else:
+                    assert pending_models is None
+    else:
+        assert pending_models is None
+
+    assert modelhub.objects_path.exists()
