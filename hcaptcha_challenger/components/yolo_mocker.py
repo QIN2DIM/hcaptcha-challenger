41f1ab1b75edd2db4215bfb0193dbadd1bd47990
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-10-14 20:02:26 +0800

    CI(PyPI): v0.8.6-post1

--- hcaptcha_challenger/components/yolo_mocker.py
+++ hcaptcha_challenger/components/yolo_mocker.py
@@ -70,6 +70,35 @@ class CcYOLO:
         return detector
 
     def spawn(self):
+        """
+        ```python
+        import os
+        import sys
+        from pathlib import Path
+
+        from hcaptcha_challenger.components.yolo_mocker import CcYOLO
+
+
+        def run():
+            model_name = "head_of_the_animal_2310_yolov8s.onnx"
+            images_dir = "image_label_area_select/please click on the head of the animal/default"
+
+            this_dir = Path(__file__).parent
+            output_dir = this_dir.joinpath("yolo_mocker")
+            images_dir = this_dir.joinpath(images_dir).absolute()
+
+            ccy = CcYOLO(model_name, images_dir, output_dir)
+            ccy.spawn()
+
+            if "win32" in sys.platform:
+                os.startfile(ccy.output_dir)
+
+
+        if __name__ == "__main__":
+            run()
+        ```
+        :return:
+        """
         def draw():
             alts = sorted(results, key=lambda x: x[-1])
             text, ps, pe, _ = alts[-1]
