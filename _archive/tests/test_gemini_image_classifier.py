4cbb11690ae79079b3ab23c37bdd52733f82f63e
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2025-03-31 07:05:12 +0800

    ff

--- _archive/tests/test_gemini_image_classifier.py
+++ _archive/tests/test_gemini_image_classifier.py
@@ -9,5 +9,10 @@ gic = GeminiImageClassifier(gemini_api_key=os.getenv("GEMINI_API_KEY"))
 
 def test_gemini_image_classifier():
     screenshot_path = "challenge_view/image_label_binary/1.png"
+
     results = gic.invoke(screenshot_path, model="gemini-2.0-flash-thinking-exp-01-21")
-    logger.debug(f'ToolInvokeMessage: {results.log_message}')
+    boolean_matrix = results.convert_box_to_boolean_matrix()
+
+    logger.debug(f"Challenge Prompt: {results.challenge_prompt}")
+    logger.debug(f"Coordinates: {results.coordinates}")
+    logger.debug(f"Results: {boolean_matrix}")
