1f25282d366aa0d20ad17c3b5d917cdf688ca5e1
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2025-04-03 20:45:43 +0800

    add tests (#999)

---
+++ tests/test_tools_spatial_path_reasoning.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+import dotenv
+from loguru import logger
+from matplotlib import pyplot as plt
+
+from hcaptcha_challenger.helper import create_coordinate_grid
+from hcaptcha_challenger.tools import SpatialPathReasoner
+
+dotenv.load_dotenv()
+gic = SpatialPathReasoner(gemini_api_key=os.getenv("GEMINI_API_KEY"))
+
+
+def test_gemini_image_classifier():
+    challenge_screenshot = Path("challenge_view/image_drag_drop/4.png")
+    grid_divisions_path = challenge_screenshot.parent.joinpath(
+        f'grid_divisions_{challenge_screenshot.name}'
+    )
+    bbox = {"x": 0, "y": 0, "width": 501, "height": 431}
+
+    grid_divisions_image = create_coordinate_grid(challenge_screenshot, bbox)
+    plt.imsave(str(grid_divisions_path.resolve()), grid_divisions_image)
+
+    results = gic.invoke(
+        challenge_screenshot=challenge_screenshot,
+        grid_divisions=grid_divisions_path,
+        model="gemini-2.5-pro-exp-03-25",
+    )
+    logger.debug(f'ToolInvokeMessage: {results.log_message}')
