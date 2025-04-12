b7d6bc8bb37c3ca3eb4c059e0c8afae2e8789cc4
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2025-04-12 18:23:16 +0800

    publish v0.15.0

---
+++ examples/demo_collector.py
@@ -0,0 +1,26 @@
+import asyncio
+from pathlib import Path
+
+from playwright.async_api import async_playwright
+
+from hcaptcha_challenger.agent.collector import Collector, CollectorConfig
+from hcaptcha_challenger.utils import SiteKey
+
+dataset_dir = Path("tmp/.cache/dataset").resolve()
+site_key = SiteKey.epic
+
+
+async def launch_collector():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+
+        collector_config = CollectorConfig(dataset_dir=dataset_dir, site_key=site_key)
+        collector = Collector(page, collector_config)
+
+        await collector.launch()
+
+
+if __name__ == '__main__':
+    asyncio.run(launch_collector())
