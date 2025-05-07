5ff9e703854f9e8fd1bd29209ea50e25d083a654
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2025-05-07 11:21:41 +0800

    Update test_tools_common.py

--- tests/test_tools_common.py
+++ tests/test_tools_common.py
@@ -1,11 +1,11 @@
 import json
 import os
 from pathlib import Path
-from uuid import uuid4
 
 import dotenv
 from google import genai
 from google.genai import types
+from google.genai.types import ThinkingConfig
 
 dotenv.load_dotenv()
 
@@ -67,3 +67,28 @@ def test_list_models():
     for model in pf.models.list():
         if "gemini-2." in model.name:
             print(model)
+
+
+def test_generate_content_fast():
+    cc = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    response = cc.models.generate_content(model="gemini-2.0-flash-lite", contents="Hello, world!")
+
+    output_path = output_dir.joinpath("generate_content_fast.json")
+    text = json.dumps(response.model_dump(mode="json"), indent=2, ensure_ascii=False)
+    output_path.write_text(text, encoding="utf8")
+
+
+def test_generate_content_reasoning():
+    cc = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    response = cc.models.generate_content(
+        model="gemini-2.5-flash-preview-04-17",
+        contents="Hello, world!",
+        config=types.GenerateContentConfig(
+            temperature=0,
+            thinking_config=ThinkingConfig(include_thoughts=False, thinking_budget=100),
+        ),
+    )
+
+    output_path = output_dir.joinpath("generate_content_non_thoughts.json")
+    text = json.dumps(response.model_dump(mode="json"), indent=2, ensure_ascii=False)
+    output_path.write_text(text, encoding="utf8")
